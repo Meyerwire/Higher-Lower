8483738ed48b6af336006c416be1826e0864581e
Meyer <jeremymakoben@yahoo.de>
2021-12-20 20:13:31 +0100

    changed into a little game
    higher lower

--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,25 @@
 from flask import Flask , render_template
+from random import randint
+
+richtige_zahl = randint(0,9)
+
+
 app = Flask(__name__)
 @app.route('/')
-def index():
+def zahlErraten():
     return render_template('index.html')
+
+@app.route('/int:nutzer_zahl')
+def richtig(nutzer_zahl):
+    if nutzer_zahl > 9 or nutzer_zahl < 0:
+        return render_template('cheat.html')
+    elif nutzer_zahl == richtige_zahl:
+        return render_template('right.html')
+    elif nutzer_zahl > richtige_zahl:
+        return render_template('lower-html')
+    elif nutzer_zahl < richtige_zahl:
+        return render_template('higher.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
